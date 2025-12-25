5339638e3d69053aa1afd970708a71d755319881
LagrothYT <61805728+LagrothYT@users.noreply.github.com>
2025-12-25 14:20:55 -0500

    Refactor create_enemies to use parameters

--- RUNECRAWL_Win64/src/enemies.cpp
+++ RUNECRAWL_Win64/src/enemies.cpp
@@ -5,10 +5,9 @@
 using namespace std;
 
 Enemies create_enemies(string name, int damage, int health) {
-        Enemies e;
-        e.name = "Orc";
-	e.damage = 5;
-	e.health = 25;
-
-        return e;
+    Enemies e;
+    e.name = name;
+    e.damage = damage;
+    e.health = health;
+    return e;
 }
