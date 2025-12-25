beb959231d6aae35ab4044468112070b2fd143cc
LagrothYT <61805728+LagrothYT@users.noreply.github.com>
2025-12-25 14:20:21 -0500

    Add 'owned' parameter to buy_weapon function

--- RUNECRAWL_Win64/src/shop.h
+++ RUNECRAWL_Win64/src/shop.h
@@ -14,9 +14,10 @@ struct weaponStore {
 	std::string name;
 	int damageAmount;
 	int cost;
+	bool owned;
 };
 
 Potion create_potion(std::string name, int healAmount, int cost, int amount);
-weaponStore buy_weapon(std::string name, int damageAmount, int cost);
+weaponStore buy_weapon(std::string name, int damageAmount, int cost, bool owned);
 
 #endif
